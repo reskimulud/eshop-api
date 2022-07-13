5145531e442bd3168c1a40d3711ba4c5f6729b1e
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-07-13 14:37:00 +0700

    initial server hapi

--- src/server.js
+++ src/server.js
@@ -1,2 +1,30 @@
 require('dotenv').config();
-console.log("Starting");
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      name: 'Rski Mulud Muchamad',
+    }),
+  })
+
+  await server.start();
+  console.log(`Server running at ${server.info.uri}`);
+
+};
+
+init()
