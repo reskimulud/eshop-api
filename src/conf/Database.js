551b7162df19b9c996226bc327fbac3355caeaa0
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-07-18 13:21:53 +0700

    membuat konfigurasi untuk koneksi ke database dan konfigurasi migration

---
+++ src/conf/Database.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+class Database {
+  constructor() {
+    this.connection = mysql.createPool({
+      connectionLimit: 10,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+  }
+
+  query(query) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, (err, result) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(result);
+      });
+    });
+  }
+}
+
+module.exports = Database;
